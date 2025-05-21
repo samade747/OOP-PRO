a026019d5aaf211348ccda8b1825965216c7872f
Abdul Samad <132301326+samade747@users.noreply.github.com>
2025-05-21 10:59:52 +0500

    1059

--- OfficalRepo/Abstraction/ABCexam1.py
+++ OfficalRepo/Abstraction/ABCexam1.py
@@ -3,4 +3,33 @@
 from abc import ABC, abstractmethod
 
 class Bank(ABC):
-    
+    @abstractmethod
+    def transacton(self):
+        pass
+
+class Deposit(Bank):
+    def transacton(self):
+        print("Deposit Transaction")
+
+class Withdraw(Bank):
+    def transacton(self):
+        print("Withdraw Transaction")
+
+# Create an instance of Deposit and call the transacton method
+
+d = Deposit()
+d.transacton()
+
+w = Withdraw()
+w.transacton()
+
+
+
+# Output: Deposit Transaction
+#         Withdraw Transaction
+
+
+# 🧠 What it teaches:
+# Bank is abstract — you can’t use it directly.
+
+# Child classes define how each transaction works.

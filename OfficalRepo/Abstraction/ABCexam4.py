a0790d6f185fbf2ccb89c8dc7a770906e48cf1bd
Abdul Samad <132301326+samade747@users.noreply.github.com>
2025-05-22 00:12:27 +0500

    Update ABCexam4.py

--- OfficalRepo/Abstraction/ABCexam4.py
+++ OfficalRepo/Abstraction/ABCexam4.py
@@ -1,3 +1,27 @@
 # ✅ 4. Payment System – Abstracting Payment Methods
 
+from abc import ABC, abstractmethod
 
+class Payment(ABC):
+    @abstractmethod
+    def pay(self, amount):
+        pass
+
+class CreditCard(Payment):
+    def pay(self, amount):
+        print(f"Paid {amount} using Credit Card.")
+
+
+class UPI(Payment):
+    def pay(self, amount):
+        print(f"Paid ₹{amount} using UPI.")
+
+# Usage
+credit = CreditCard()
+credit.pay(500)
+
+upi = UPI()
+upi.pay(250)
+
+# 🧠 What it teaches:
+# User calls pay() without worrying about how it’s done.
